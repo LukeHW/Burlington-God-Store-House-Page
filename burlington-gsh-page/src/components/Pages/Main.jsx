464166b5f8f5193b3e4ab2c7f90989a5ec695217
Luke Wahlmeier <luke2015@ksu.edu>
2021-05-31 13:14:59 -0500

    Grammatical Changes

--- burlington-gsh-page/src/components/Pages/Main.jsx
+++ burlington-gsh-page/src/components/Pages/Main.jsx
@@ -226,10 +226,10 @@ function Main() {
             <Grid item xs="auto" sm="auto" md={1} lg={2} xl={2} />
           <Grid item className={classes.spacer} xs={12} sm={12} md={10} lg={8} xl={8}>
               <Typography className={classes.spacerTextBody} variant="h4" component="h6" gutterBottom>
-                At God&#39;s Storehouse in Burlington, Kansas, we serve residents living in Coffey County who find themselves in need. God&#39;s Storehouse operates under the Coffey County Ministerial Association. In April of 2020 GSH celebrated their 15th year of service in Coffey County. Our current address is 515 Neosho Street in Burlington, Kansas. In 2018 our current building was donated to us. Our hours are Monday thru Friday, 9am – 12pm, and every Tuesday evening 5pm - 7pm. God has blessed us with 35 to 40 volunteers who operate our store.
+                At God&#39;s Storehouse in Burlington, Kansas, we serve residents living in Coffey County who find themselves in need. God&#39;s Storehouse operates under the Coffey County Evangelical Ministerial Association. In 2018 our current building was donated to us. In April of 2020 GSH celebrated their 15th year of service in Coffey County. Our current address is 515 Neosho Street in Burlington, Kansas. Our hours are Monday thru Friday, 9am – 12pm, and every Tuesday evening 5pm - 7pm. God has blessed us with 35 to 40 volunteers who operate our store.
               </Typography>
               <Typography className={classes.spacerTextBody} variant="h4" component="h6" gutterBottom>
-                We utilize a card system to record contact and family information. Families can receive food once a month according to our designated guidelines. Along with food we also provide: clothing, shoes, bedding, pots, pans, plates, drinking glasses, and silverware that are donated from the various churches, schools, organizations, and individuals from Coffey County. Consumers can shop as often as they choose for these items. Several churches and individuals donate money monthly. We operate solely from donations.
+                We utilize a card system to record contact and family information. Families can receive food once a month according to our designated guidelines. Along with food we also provide: clothing, shoes, bedding, pots, pans, plates, drinking glasses, and silverware that are donated from the various churches, schools, organizations, and individuals from Coffey County. Several churches and individuals donate money monthly. We operate solely from donations.
               </Typography>
               <Typography className={classes.spacerTextBody} variant="h4" component="h6" gutterBottom>
                 We average around 139 families/390 people per month.
@@ -251,7 +251,8 @@ function Main() {
                 Clothes
               </Typography>
               <Typography className={classes.infoBody} variant="h1" component="h2" gutterBottom align='center'>
-                God&#39;s Storehouse offers a wide variety of clothing items to Coffey County Residents who find themselves in need.  Items include infants clothing, school age children clothing as well as men&#39;s and women&#39;s items.  We have shoes available as well.  Socks, underwear, sheets, blankets, towels and washclothes are available by request.  These items can only be requested once a year as these items are limited.              </Typography>
+                God&#39;s Storehouse offers a wide variety of clothing items to Coffey County Residents who find themselves in need.  Items include infant&#39;s clothing, school age children&#39;s clothing as well as men&#39;s and women&#39;s items.  We have shoes available as well.  Socks, underwear, sheets, blankets, towels and washclothes are available by request.  These items can only be requested once a year as these items are limited.              
+              </Typography>
           </Grid>
           <Grid item className={classes.infoSpace} xs={12} sm={12} md={12} lg={4} xl={4}>
               <img className={classes.icons} src="/foodIcon.png" alt="Food and Produce" />
@@ -259,7 +260,7 @@ function Main() {
                 Food
               </Typography>
               <Typography className={classes.infoBody} variant="h1" component="h2" gutterBottom align='center'>
-                God&#39;s Storehouse has food available for any Coffey County Resident who is in need.  We offer breakfast items, lunch & dinner, canned fruit, misc. items and canned vegetables.  We also have hygeine items available such as toothpaste, toothbrushes, bar soap, dish soap, deodarant, shampoo, toilet paper and laundry detergent; these items are not constant, only when available.
+                God&#39;s Storehouse has food available for any Coffey County Resident who is in need.  We offer breakfast items, lunch & dinner items, canned fruit, misc. items and canned vegetables.  We also have hygeine items available such as toothpaste, toothbrushes, bar soap, dish soap, deodorant, shampoo, toilet paper and laundry detergent; these items are not constant, only while supplies last.
               </Typography>
           </Grid>
           <Grid item className={classes.infoSpace} xs={12} sm={12} md={12} lg={3} xl={3}>
