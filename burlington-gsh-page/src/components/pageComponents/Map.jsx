118fdf7180793b169cd1beaacb429d15280ceedb
Luke Wahlmeier <luke2015@ksu.edu>
2022-10-26 19:43:39 -0600

    Updated map for location page

--- burlington-gsh-page/src/components/pageComponents/Map.jsx
+++ burlington-gsh-page/src/components/pageComponents/Map.jsx
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
-import GoogleMapReact from 'google-map-react';
+/* eslint-disable no-shadow */
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const AnyReactComponent = () => <div style={{ marginLeft: '-15px', marginTop: '-55px' }}><img width='30px' height='60px' src="/googleMapIcon.svg" alt="Our Location" /></div>;
+const center = {
+  lat: 38.194402,
+  lng: -95.7409615
+};
+
+const style = { 
+  height: '100%', 
+  width: '100%',
+};
 
 function Map() { 
 
-  const [start, setStart] = useState({
-    center: {
-      lat: 38.194576,
-      lng: -95.74085
-    },
-    zoom: 17
-  });
-
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100%', width: '100%', align: 'center' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAVgbRI-G5CNsfM9sT_aOQp2gJ6gRT7sKw" }}
-        defaultCenter={start.center}
-        defaultZoom={start.zoom}
-        yesIWantToUseGoogleMapApiInternals
+    <LoadScript googleMapsApiKey='AIzaSyDnfUEw60TW9NJRkKOu72OaxucqKkmwmVo'>
+      <GoogleMap
+        mapContainerStyle={style}
+        center={center}
+        zoom={17}
       >
-        <AnyReactComponent
-          lat={38.194446}
-          lng={-95.74090}
-          text="God's Storehouse"
-        />
-      </GoogleMapReact>
-    </div>
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
